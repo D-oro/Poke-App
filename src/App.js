2f491466de810c8b46d72bedd7d650433c94b543
D-oro <107217252+D-oro@users.noreply.github.com>
2022-11-02 21:33:42 +0000

    need to fix fetch blocked by CORS policy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { NavBar } from './components/NavBar';
 
-function App() {
+const App = ()=> {
+
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect (() =>{
+    fetchPokemon();
+  })
+
+  const fetchPokemon = async () => {
+    for (let i = 1; i < 10; i++) {
+      const response = await fetch(`https://pokeapi.co/v2/pokemon/1`
+      , {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+    const data = await response.json();
+    console.log(data);
+   }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+
+      <ul>
+      {pokemon.map((pok) => (
+      <div>
+        <img src={pok.sprites.front_default}/>
+        <li>{pok.name}</li>
+      </div>
+      ))}
+      </ul>
+
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div className="h-10 w-full bg-green-500 shadow-xl text-emerald-800 text-center">The best Pokemon app ever!</div>
+  )
+}
