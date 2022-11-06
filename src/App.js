116bb20c9dd8887c540b3bc39854ec1b4b77c972
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-06 09:21:39 -0500

    Adjust styles

--- src/App.js
+++ src/App.js
@@ -21,19 +21,19 @@ const App = () => {
   };
 
   return (
-    <div className='bg-[#AFE2E9]'>
+    <div className='h-full bg-[#AFE2E9]'>
       <NavBar />
-      <ul className='grid grid-cols-6 my-10 mx-10'>
+      <ul className='grid grid-cols-6 mx-10'>
         {pokemon.map((pok) => (
-          <div key={pok.name} className="mx-5 my-5 bg-[#217394] bg-opacity-50 backdrop-blur-sm">
+          <div key={pok.name} className="mx-5 my-5 bg-[#217394] bg-opacity-50 backdrop-blur-sm rounded">
             {/* {fetchSpecies(pok.url)} */}
             <li className='shadow-md px-5 text-center'>
               <img className='w-32 mx-auto' src={pok.sprites.front_default} alt={pok.name} />
-              {pok.name.substring(0, 1).toUpperCase() + pok.name.substring(1)}
+              <p className='text-white font-bold text-xl'>{pok.name.substring(0, 1).toUpperCase() + pok.name.substring(1)}</p>
               <p className='my-5'>Abilities</p>
-              <ul className='flex justify-center'>
+              <ul className='flex justify-center mb-2'>
                 {pok.abilities.map((ability) => (
-                  <li className='mx-2 rounded px-2 py-1' key={ability.ability.name}>{ability.ability.name}</li>
+                  <li className='mx-2 rounded-md px-2 py-1 bg-gradient-to-r  from-[#E4504E] via-[#dc7d7b] to-[#eaa2a1] bg-opacity-75 text-white' key={ability.ability.name}>{ability.ability.name}</li>
                 ))}
               </ul>
             </li>
