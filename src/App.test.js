31bbe0d102be7369b40b03dfba6c4b8cc07d005c
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-06 11:17:26 -0500

    Add snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const tree = render(<App />);
+  expect(tree).toMatchSnapshot();
 });
