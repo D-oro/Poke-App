7fcaeec0d5bfbaa7b9115d70557625bc06f46434
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-06 09:13:02 -0500

    Fix CORS issue

--- src/App.js
+++ src/App.js
@@ -2,41 +2,43 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import { NavBar } from './components/NavBar';
 
-const App = ()=> {
+const App = () => {
 
   const [pokemon, setPokemon] = useState([])
 
-  useEffect (() =>{
+  useEffect(() => {
     fetchPokemon();
-  })
+  }, [])
 
   const fetchPokemon = async () => {
-    for (let i = 1; i < 10; i++) {
-      const response = await fetch(`https://pokeapi.co/v2/pokemon/1`
-      , {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-    const data = await response.json();
-    console.log(data);
-   }
+    const data = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+    const pokemon = await data.json();
+    pokemon.results.forEach(async (pokemon) => {
+      const data = await fetch(pokemon.url);
+      const pokemonData = await data.json();
+      setPokemon((currentPokemon) => [...currentPokemon, pokemonData])
+    })
   };
 
   return (
-    <div>
-      <NavBar/>
-
-      <ul>
-      {pokemon.map((pok) => (
-      <div>
-        <img src={pok.sprites.front_default}/>
-        <li>{pok.name}</li>
-      </div>
-      ))}
+    <div className='bg-[#AFE2E9]'>
+      <NavBar />
+      <ul className='grid grid-cols-6 my-10 mx-10'>
+        {pokemon.map((pok) => (
+          <div key={pok.name} className="mx-5 my-5 bg-[#217394] bg-opacity-50 backdrop-blur-sm">
+            {/* {fetchSpecies(pok.url)} */}
+            <li className='shadow-md px-5 text-center'>
+              <img className='w-32 mx-auto' src={pok.sprites.front_default} alt={pok.name} />
+              {pok.name.substring(0, 1).toUpperCase() + pok.name.substring(1)}
+              <p className='my-5'>Abilities</p>
+              <ul className='flex justify-center'>
+                {pok.abilities.map((ability) => (
+                  <li className='mx-2 rounded px-2 py-1' key={ability.ability.name}>{ability.ability.name}</li>
+                ))}
+              </ul>
+            </li>
+          </div>
+        ))}
       </ul>
 
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const NavBar = () => {
   return (
-    <div className="h-10 w-full bg-green-500 shadow-xl text-emerald-800 text-center">The best Pokemon app ever!</div>
+    <div className="h-10 w-full bg-[#AFE2E9] shadow-xl text-emerald-800 text-center">The best Pokemon app ever!</div>
   )
 }
